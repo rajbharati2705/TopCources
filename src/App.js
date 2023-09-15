ce14c4bc8313f0a1f8e7f401c415fe2fa15ab379
rajbharati2705 <rajbharati2705@gmail.com>
2023-09-15 15:48:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import NavBar from "./components/NavBar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import {filterData} from "./data";
+import Spinner from './components/Spinner';
+import {toast} from "react-toastify";
 
-function App() {
+
+const App = () => {
+
+  const [courses,setCourses]=useState(null);
+  const [loading,setLoading]=useState(true);
+  const [category,setCategory]=useState(filterData[0].title);
+  async function fetchData(){
+    setLoading(true)
+    try{
+      let response=await fetch("https://codehelp-apis.vercel.app/api/get-top-courses");
+      let output=await response.json();
+      setCourses(output.data);
+    }
+    catch (error){
+        toast.error("Network Error");
+    }
+    setLoading(false);
+  }
+  useEffect( ()=>{
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen flex flex-col bg-red-400'>
+      <div >
+        <NavBar></NavBar>
+      </div>
+      <div >
+      <div>
+        <Filter  category={category}
+        setCategory={setCategory}
+        filterData={filterData}></Filter>
+      </div>
+      <div className='w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]'>
+        {
+          loading? (<Spinner></Spinner>) : (<Cards courses={courses} category={category} ></Cards>)
+        }
+      </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='bg-indigo-500 py-4'>
+        <h1 className='text-3xl font-bold text-center '>Top Course</h1>
+    </div>
+  )
+}
+
+export default NavBar
